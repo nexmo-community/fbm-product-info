6aac637ae0b17ab3865ec730f93101c3c806bccf
Tony Bedford <tony.bedford@vonage.com>
2019-04-09 18:33:45 +0100

    Testing out automode

--- ProductMatcher/ProductMatcher.py
+++ ProductMatcher/ProductMatcher.py
@@ -2,14 +2,16 @@ from FBMClient.FBMClient import FBMClient
 
 class ProductMatcher:
 
+    auto_mode = True
+
     cats_dict = {
-        'none': 'Try `info: toys | food | meds | bling`',
+        'other': 'Our products: toys, food, meds, and bling',
         'toys': 'More info on cat toys here https://bit.ly/abc',
         'food': 'More info on cat food here https://bit.ly/def',
         'meds': 'More info on cat meds here https://bit.ly/ghi',
         'bling': 'More info on cat bling here https://bit.ly/jkl'
     }
-    
+
     def __init__(self, nexmo_app_id):
         filename = "private.key"
         jwt_expiry = 1*60*60 # JWT expires after one hour (default is 15 minutes)
@@ -17,16 +19,20 @@ class ProductMatcher:
         return
 
     def product_matcher(self, fb_sender, user, msg):
+        product = 'other'
         msg = msg.lower().strip()
-        if "info:" not in msg:
-            product = 'none' 
-        else:
-            msg = msg.split(':')[1]
+        if self.auto_mode:
+            if "auto: off" in msg:
+                self.auto_mode = False
+                self.fbm.send_message(fb_sender, user, "Auto mode is off")
+                return
             for k in self.cats_dict.keys():
                 if k in msg:
                     product = k
                     break
-        self.fbm.send_message(fb_sender, user, self.cats_dict[product])
-        return product    
-
-
+            self.fbm.send_message(fb_sender, user, self.cats_dict[product])
+        if "auto: on" in msg:
+                self.auto_mode = True
+                self.fbm.send_message(fb_sender, user, "Auto mode is on")
+        print("Product: %s" % product)
+        return product
