87791d1dba948d4a20e77454394bb6c5a3ba3dcb
Tony Bedford <tony.bedford@vonage.com>
2019-04-10 10:51:39 +0100

    Small change

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ def inbound_message():
     fb_sender = data['to']['id']
     in_msg = data['message']['content']['text']
     pm.product_matcher(fb_sender, user, in_msg)
+    print("DEBUG: Return from Message sent.")
     return ("200")
 
 @app.route('/status', methods=['POST'])
 def message_status():
     print ("Message status:")    
     data = request.get_json()
-    pprint(data)
+    #pprint(data)
     return ("200")
 
 if __name__ == '__main__':
